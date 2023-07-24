7ebe081da51392e6b0206c8cadbef5cce37d306f
AlexanderLivanov <help.underlinegroup@gmail.com>
2023-07-24 13:46:40 +0300

    кнопка удаления фото jquery

--- a/sys/cms/gallery.php
+++ a/sys/cms/gallery.php
@@ -75,6 +75,7 @@ if (!empty($_GET['err'])) {
         }
 
         .img-cont:hover .img-button {
+            cursor: pointer;
             position: absolute;
             display: flex;
             transition: all 0.3s;
@@ -129,7 +130,7 @@ if (!empty($_GET['err'])) {
             echo '<div class="img-cont">
                     <img id="' . $image . '" class="gallery-img" src="' . $image . '" alt="">
                     <div class="img-button">
-                        <a href="#" onclick="deleteThisPhoto(\'' . $image . '\')" id="deleteThisPhoto" style="box-shadow: 0 0 15px grey; color: grey; background-color: rgba(255, 255, 255, 1); border: white; border-radius: 5px; padding: 2px; text-align: center; verticle-align: middle;">
+                        <a onclick="deleteThisPhoto(this.id)" id="' . $image . '" style="box-shadow: 0 0 15px grey; color: grey; background-color: rgba(255, 255, 255, 1); border: white; border-radius: 5px; padding: 2px; text-align: center; verticle-align: middle;">
                         <svg xmlns="http://www.w3.org/2000/svg" style="" class="icon icon-tabler icon-tabler-trash-filled" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z" stroke-width="0" fill="currentColor"></path>
@@ -144,13 +145,14 @@ if (!empty($_GET['err'])) {
 
     <script>
         function deleteThisPhoto(photoID) {
+            document.getElementById(photoID).style.display = "none";
             jQuery.ajax({
                 type: 'POST',
                 data: $(this).serialize(),
                 dataType: 'html',
                 url: 'content/delete.php?photoID=' + photoID,
                 success: function(html) {
-                    $("'" + JSON.stringify('#' + photoID) + "'").hide();
+                    console.log('photo deleted');
                 }
             });
         }
