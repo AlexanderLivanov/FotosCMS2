2715452fc922763dd87caaa34c7b0128d52ce4c6
AlexanderLivanov <help.underlinegroup@gmail.com>
2023-08-17 22:28:11 +0300

    just test

--- a/sys/cms/my.php
+++ a/sys/cms/my.php
@@ -4,8 +4,10 @@ $query->execute();
 $result = $query->fetch(PDO::FETCH_ASSOC);
 
 echo $_SESSION['user_name'] . ' — Профиль | Участник сообщества с ' . $result['registered'] . ' &nbsp;';
-echo '<form method="post"><input type="submit" name="logout" value="Выйти"/></form>';
 if ($result['admin'] == 1) {
     echo '<p style="color: red;">Вы находитесь в учётной записи администратора!</p>';
 }
+
+echo '<form method="post"><input type="submit" name="logout" value="Выйти"/></form>';
+
 ?>
