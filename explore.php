8bff4a29ee4b7008b238e73bc64c7ea89cfb742f
AlexanderLivanov <help.underlinegroup@gmail.com>
2023-09-01 22:25:51 +0300

    поиск пользователей

--- explore.php
+++ explore.php
@@ -151,72 +151,66 @@ $uname = "";
 
 if (!empty($_POST['username']) or !empty($_GET['u'])) {
     if(!empty($_POST['username'])){
-        $directory = "content/" . $_POST['username'];
         $srch = $_POST['username'];
     }
     if(!empty($_GET['u'])){
-        $directory = "content/" . $_GET['u'];
         $srch = $_GET['u'];
     }
-    if (is_dir($directory)) {
-        require_once('a/sys/header-fixed.php');
-        $query = $connection->prepare("SELECT * FROM users where `username` = '" . $srch . "'");
-        $query->execute();
-        $result = $query->fetch(PDO::FETCH_ASSOC);
+    require_once('a/sys/header-fixed.php');
 
-        $datestr = $result['registered'];
-        $datetime = strtotime($datestr);
-        $diff = time() - $datetime;
-        $days = intval($diff / 86400);
-        
-        echo ('<div style="border-radius: 5px; text-align: center; padding: 1em;">');
-        echo('<div id="profile-banner">');
-        echo('
-            <div id="profile-avatar">
-                <img src="https://cdnn1.inosmi.ru/images/23538/30/235383059.jpg">
-            </div>
-        ');
-        echo('<div style="padding: 1em;">');
-        echo('<h1>' . $result['username'] . '</h1>');
-        echo('<h3>Участник сообщества с ' . $result['registered'] . "</h3><h3> (" . $days . ' дней назад) &nbsp;</h3>');
-        echo('</div>');
-        echo('</div>');
-        $images = glob($directory . "/*");
-        echo ('<div class="container">');
-        foreach ($images as $image) {
-            echo '<div><img class="gallery-item" id="' . $image . '" src="' . $image . '" alt=""></div>';
+    function printUser($value){
+        foreach($value as $user){
+            echo $user[1] . '<br>';
         }
-        echo ('</div>');
-        echo ('</div>');
-    } else {
-        echo ('
-        <head>
-            <link rel="stylesheet" href="css/search.css">
-        </head>
-        <div class="search" style="text-align: center; margin-top: 20%; background-color: whitesmoke; height: 100px; margin-right: 10%; margin-left: 10%; padding: 1em; border-radius: 40px;">
-            <p>Упс... Кажется, такого пользователя нет... Попробуйте повторить поиск:</p>
-            <form action="explore" method="POST">
-                <input type="text" value="' . $srch . '" name="username" pattern="[a-zA-Z0-9]+">
-                <input type="submit" value="Поиск" id="search-btn"><br>
-                <a href="/" style="text-decoration: none; color: #0072ff;">На главную...</a>
-            </form>
-        </div>
-        ');
     }
+
+    $quest = "SELECT * FROM `users` WHERE username LIKE '$srch%'";
+    $request = $connection->prepare($quest);
+    $request->execute();
+
+    $result = $request->fetchAll();
+    printUser($result);
+
+    $datestr = $result['registered'];
+    $datetime = strtotime($datestr);
+    $diff = time() - $datetime;
+    $days = intval($diff / 86400);
+    
+    // echo($srch);
+    // print_r($result);
+    // echo ('<div style="border-radius: 5px; text-align: center; padding: 1em;">');
+    // echo('<div id="profile-banner">');
+    // echo('
+    //     <div id="profile-avatar">
+    //         <img src="https://cdnn1.inosmi.ru/images/23538/30/235383059.jpg">
+    //     </div>
+    // ');
+    // echo('<div style="padding: 1em;">');
+    // echo('<h1>' . $result['username'] . '</h1>');
+    // echo('<h3>Участник сообщества с ' . $result['registered'] . "</h3><h3> (" . $days . ' дней назад) &nbsp;</h3>');
+    // echo('</div>');
+    // echo('</div>');
+    // $images = glob($directory . "/*");
+    // echo ('<div class="container">');
+    // foreach ($images as $image) {
+    //     echo '<div><img class="gallery-item" id="' . $image . '" src="' . $image . '" alt=""></div>';
+    // }
+    // echo ('</div>');
+    // echo ('</div>');
 } else {
     echo ('
-        <head>
-            <link rel="stylesheet" href="css/search.css">
-        </head>
-        <div class="search" style="text-align: center; margin-top: 20%; background-color: whitesmoke; height: 100px; margin-right: 10%; margin-left: 10%; padding: 1em; border-radius: 40px;">
-            <p>Кажется, вы что-то неправильно ввели... Попробуйте повторить поиск:</p>
-            <form action="explore" method="POST">
-                <input type="text" value="' . $srch . '" name="username" pattern="[a-zA-Z0-9]+">
-                <input type="submit" value="Поиск" id="search-btn"><br>
-                <a href="/" style="text-decoration: none; color: #0072ff;">На главную...</a>
-            </form>
-        </div>
-        ');
+    <head>
+        <link rel="stylesheet" href="css/search.css">
+    </head>
+    <div class="search" style="text-align: center; margin-top: 20%; background-color: whitesmoke; height: 100px; margin-right: 10%; margin-left: 10%; padding: 1em; border-radius: 40px;">
+        <p>Упс... Кажется, такого пользователя нет... Попробуйте повторить поиск:</p>
+        <form action="explore" method="POST">
+            <input type="text" value="' . $srch . '" name="username" pattern="[a-zA-Z0-9]+">
+            <input type="submit" value="Поиск" id="search-btn"><br>
+            <a href="/" style="text-decoration: none; color: #0072ff;">На главную...</a>
+        </form>
+    </div>
+    ');
 }
 
 ?>
